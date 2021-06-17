3875419c57b86c2a6d907d6c37d45798db02deb7
AntonNikonov1 <125year125@gmail.com>
2021-06-18 00:48:02 +0300

    deploy

--- src/Router/routerKeys.js
+++
@@ -1,7 +0,0 @@
-export const ROUTER_KEY = {
-  HOME: '/',
-  SPEED_BOAT: '/speed-boats',
-  BATH_SAUNA: '/bath-sauna',
-  WATERS_SLIDES: '/waterslides',
-  SLOT_MACHINES: '/slot-machines',
-}
--- src/components/Title/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Title = (props) => (
-  <h2 {...props} className={props.isIntro ? 'intro-title' : 'title'}>
-    {props.children}
-  </h2>
-)
