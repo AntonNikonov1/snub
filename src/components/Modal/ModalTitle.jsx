3565537af68c24e70a134de4aee9593b7a16547e
AntonNikonov1 <125year125@gmail.com>
2021-06-18 15:56:27 +0300

    prototype

--- src/components/Modal/ModalTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const ModalTitle = ({ children }) => {
-  return (
-    <h2 className="modal__title-wrapper">
-      <span className="modal__title-text">{children}</span>
-    </h2>
-  )
-}
--- src/utils/bodyScroll.js
+++
@@ -1,7 +0,0 @@
-export const bodyScroll = (isScroll) => {
-  if (isScroll) {
-    document.body.style.overflow = 'hidden'
-  } else {
-    document.body.style.overflow = 'unset'
-  }
-}
